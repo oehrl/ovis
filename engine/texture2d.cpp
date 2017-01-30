2c9471f1eab5b6dbe9ecbba5a782b33236c73daf
Simon Oehrl <simon.oehrl@gmail.com>
2017-01-30 01:33:24 +0100

    fixed description getter and added new texture formats as well as texture filter options

--- engine/texture2d.cpp
+++ engine/texture2d.cpp
@@ -10,23 +10,114 @@ Texture2D::Texture2D(
     const Texture2DDescription& description,
     const void* pixels
 ) :
-    Texture(graphics_device)
+    Texture(graphics_device),
+    m_description(description)
 {
     Bind(0);
+    
+    GLenum internal_format;
+    GLenum source_format;
+    GLenum source_type;
+    switch (description.format)
+    {
+        case TextureFormat::RGB_UINT8:
+            internal_format = GL_RGB;
+            source_format = GL_RGB;
+            source_type = GL_UNSIGNED_BYTE;
+            break;
+            
+        case TextureFormat::RGBA_UINT8:
+            internal_format = GL_RGBA;
+            source_format = GL_RGBA;
+            source_type = GL_UNSIGNED_BYTE;
+            break;
+
+        default:
+            SDL_assert(false);
+            break;
+    }
+    
     glTexImage2D(
         GL_TEXTURE_2D,
         0,
-        GL_RGB,
+        internal_format,
         static_cast<GLsizei>(description.width),
         static_cast<GLsizei>(description.height),
         0,
-        GL_RGB,
-        GL_UNSIGNED_BYTE,
+        source_format,
+        source_type,
         pixels
     );
-    glGenerateMipmap(GL_TEXTURE_2D);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    GLenum min_filter;
+    GLenum mag_filter;
+    switch (description.filter)
+    {
+        case TextureFilter::POINT:
+            min_filter = description.mip_map_count > 1 ? GL_NEAREST_MIPMAP_NEAREST : GL_NEAREST;
+            mag_filter = GL_NEAREST;
+            break;
+            
+        case TextureFilter::BILINEAR:
+            min_filter = description.mip_map_count > 1 ? GL_LINEAR_MIPMAP_NEAREST : GL_LINEAR;
+            mag_filter = GL_LINEAR;
+            break;
+            
+        case TextureFilter::TRILINEAR:
+            SDL_assert(description.mip_map_count > 1);
+            min_filter = GL_LINEAR_MIPMAP_LINEAR;
+            mag_filter = GL_LINEAR;
+            break;
+
+        default:
+            SDL_assert(false);
+            break;
+    }
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, min_filter);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mag_filter);
+}
+
+void Texture2D::Write(
+    std::size_t level,
+    std::size_t x,
+    std::size_t y,
+    std::size_t width,
+    std::size_t height,
+    const void* data
+)
+{
+    Bind(0);
+    
+    GLenum source_format;
+    GLenum source_type;
+    switch (m_description.format)
+    {
+        case TextureFormat::RGB_UINT8:
+            source_format = GL_RGB;
+            source_type = GL_UNSIGNED_BYTE;
+            break;
+            
+        case TextureFormat::RGBA_UINT8:
+            source_format = GL_RGBA;
+            source_type = GL_UNSIGNED_BYTE;
+            break;
+
+        default:
+            SDL_assert(false);
+            break;
+    }
+    
+    glTexSubImage2D(
+        GL_TEXTURE_2D,
+        static_cast<GLsizei>(level),
+        static_cast<GLsizei>(x),
+        static_cast<GLsizei>(y),
+        static_cast<GLsizei>(width),
+        static_cast<GLsizei>(height),
+        source_format,
+        source_type,
+        data
+    );
 }
 
 void Texture2D::Bind(Uint32 texture_unit)
--- engine/texture2d.hpp
+++ engine/texture2d.hpp
@@ -8,7 +8,15 @@
 
 enum class TextureFormat
 {
-    RGB_UINT8
+    RGB_UINT8,
+    RGBA_UINT8,
+};
+
+enum class TextureFilter
+{
+    POINT,
+    BILINEAR,
+    TRILINEAR,
 };
 
 struct Texture2DDescription
@@ -17,6 +25,7 @@ struct Texture2DDescription
     std::size_t height;
     std::size_t mip_map_count;
     TextureFormat format;
+    TextureFilter filter;
 };
 
 class Texture2D :
@@ -27,6 +36,17 @@ public:
         GraphicsDevice* graphics_device,
         const Texture2DDescription& description,
         const void* pixels = nullptr
+    );
+    
+    void GenerateMipMaps();
+    
+    void Write(
+        std::size_t level,
+        std::size_t x,
+        std::size_t y,
+        std::size_t width,
+        std::size_t height,
+        const void* data
     );
     
     inline const Texture2DDescription& description() const
