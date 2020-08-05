66fd1c4356900d8ed79e58aaae4a73c6de3d5b52
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2020-08-05 12:20:44 +0200

    Add implementation for RenderBefore and RenderAfter

--- ovis/rendering/render_pass.cpp
+++ ovis/rendering/render_pass.cpp
@@ -3,13 +3,20 @@
 
 namespace ovis {
 
-RenderPass::RenderPass(const std::string& name)
-    : name_(name) {}
+RenderPass::RenderPass(const std::string& name) : name_(name) {}
 
 RenderPass::~RenderPass() {
-    if (render_pipeline_ != nullptr) {
-        render_pipeline_->RemoveRenderPass(this);
-    }
+  if (render_pipeline_ != nullptr) {
+    render_pipeline_->RemoveRenderPass(this);
+  }
+}
+
+void RenderPass::RenderBefore(const std::string& render_pass_name) {
+  render_before_list_.insert(render_pass_name);
+}
+
+void RenderPass::RenderAfter(const std::string& render_pass_name) {
+  render_after_list_.insert(render_pass_name);
 }
 
 }  // namespace ovis
