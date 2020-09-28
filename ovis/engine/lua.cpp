107bea4b622ee05d4f230965cc0369c65bdc425a
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2020-09-28 22:39:00 +0200

    Add operators for vector types to lua

--- ovis/engine/lua.cpp
+++ ovis/engine/lua.cpp
@@ -28,12 +28,42 @@ void Lua::SetupEnvironment() {
       state.new_usertype<glm::vec2>("Vector2", sol::constructors<glm::vec2(), glm::vec2(float, float)>());
   vector2_type["x"] = &glm::vec2::x;
   vector2_type["y"] = &glm::vec2::y;
+  vector2_type[sol::meta_function::unary_minus] = [](const glm::vec2& vector) { return -vector; };
+  vector2_type[sol::meta_function::addition] = [](const glm::vec2& lhs, const glm::vec2& rhs) { return lhs + rhs; };
+  vector2_type[sol::meta_function::subtraction] = [](const glm::vec2& lhs, const glm::vec2& rhs) { return lhs - rhs; };
+  vector2_type[sol::meta_function::multiplication] =
+      sol::overload([](double lhs, const glm::vec2& rhs) { return static_cast<float>(lhs) * rhs; },
+                    [](const glm::vec2& lhs, double rhs) { return lhs * static_cast<float>(rhs); });
+  vector2_type[sol::meta_function::division] = [](const glm::vec2& lhs, double rhs) {
+    return lhs / static_cast<float>(rhs);
+  };
+  vector2_type[sol::meta_function::to_string] = [](const glm::vec2& vector) {
+    return "(" + std::to_string(vector.x) + "," + std::to_string(vector.y) + ")";
+  };
+  vector2_type[sol::meta_function::length] = [](const glm::vec2& vector) { return 2; };
+  vector2_type["length"] = sol::property([](const glm::vec2& vector) { return glm::length(vector); });
+  vector2_type["lengthSq"] = sol::property([](const glm::vec2& vector) { return glm::dot(vector, vector); });
 
   sol::usertype<glm::vec3> vector3_type =
       state.new_usertype<glm::vec3>("Vector3", sol::constructors<glm::vec3(), glm::vec3(float, float, float)>());
   vector3_type["x"] = &glm::vec3::x;
   vector3_type["y"] = &glm::vec3::y;
   vector3_type["z"] = &glm::vec3::z;
+  vector3_type[sol::meta_function::unary_minus] = [](const glm::vec3& vector) { return -vector; };
+  vector3_type[sol::meta_function::addition] = [](const glm::vec3& lhs, const glm::vec3& rhs) { return lhs + rhs; };
+  vector3_type[sol::meta_function::subtraction] = [](const glm::vec3& lhs, const glm::vec3& rhs) { return lhs - rhs; };
+  vector3_type[sol::meta_function::multiplication] =
+      sol::overload([](double lhs, const glm::vec3& rhs) { return static_cast<float>(lhs) * rhs; },
+                    [](const glm::vec3& lhs, double rhs) { return lhs * static_cast<float>(rhs); });
+  vector3_type[sol::meta_function::division] = [](const glm::vec3& lhs, double rhs) {
+    return lhs / static_cast<float>(rhs);
+  };
+  vector3_type[sol::meta_function::to_string] = [](const glm::vec3& vector) {
+    return "(" + std::to_string(vector.x) + "," + std::to_string(vector.y) + "," + std::to_string(vector.z) + ")";
+  };
+  vector3_type[sol::meta_function::length] = [](const glm::vec3& vector) { return 3; };
+  vector3_type["length"] = sol::property([](const glm::vec3& vector) { return glm::length(vector); });
+  vector3_type["lengthSq"] = sol::property([](const glm::vec3& vector) { return glm::dot(vector, vector); });
 
   sol::usertype<glm::vec4> vector4_type =
       state.new_usertype<glm::vec4>("Vector4", sol::constructors<glm::vec4(), glm::vec4(float, float, float, float)>());
@@ -41,6 +71,22 @@ void Lua::SetupEnvironment() {
   vector4_type["y"] = &glm::vec4::y;
   vector4_type["z"] = &glm::vec4::z;
   vector4_type["w"] = &glm::vec4::w;
+  vector4_type[sol::meta_function::unary_minus] = [](const glm::vec4& vector) { return -vector; };
+  vector4_type[sol::meta_function::addition] = [](const glm::vec4& lhs, const glm::vec4& rhs) { return lhs + rhs; };
+  vector4_type[sol::meta_function::subtraction] = [](const glm::vec4& lhs, const glm::vec4& rhs) { return lhs - rhs; };
+  vector4_type[sol::meta_function::multiplication] =
+      sol::overload([](double lhs, const glm::vec4& rhs) { return static_cast<float>(lhs) * rhs; },
+                    [](const glm::vec4& lhs, double rhs) { return lhs * static_cast<float>(rhs); });
+  vector4_type[sol::meta_function::division] = [](const glm::vec4& lhs, double rhs) {
+    return lhs / static_cast<float>(rhs);
+  };
+  vector4_type[sol::meta_function::to_string] = [](const glm::vec4& vector) {
+    return "(" + std::to_string(vector.x) + "," + std::to_string(vector.y) + "," + std::to_string(vector.z) + "," +
+           std::to_string(vector.w) + ")";
+  };
+  vector4_type[sol::meta_function::length] = [](const glm::vec4& vector) { return 4; };
+  vector4_type["length"] = sol::property([](const glm::vec4& vector) { return glm::length(vector); });
+  vector4_type["lengthSq"] = sol::property([](const glm::vec4& vector) { return glm::dot(vector, vector); });
 
   sol::usertype<Scene> scene_type = state.new_usertype<Scene>("Scene");
   scene_type["CreateObject"] = static_cast<SceneObject* (Scene::*)(const std::string&)>(&Scene::CreateObject);
